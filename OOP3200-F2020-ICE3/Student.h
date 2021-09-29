ba5a3d727f5980beca82dc529720714378a00297
Devanshi <pdevanshi616@gmail.com>
2021-09-29 11:21:55 -0400

    Updated Student class

--- OOP3200-F2020-ICE3/Student.h
+++ OOP3200-F2020-ICE3/Student.h
@@ -4,10 +4,11 @@
  * @version 1.0
  */
 
+#pragma once
 
 #ifndef _STUDENT_H
 #define _STUDENT_H
-
+#include <string>
 #include "Person.h"
 
 
@@ -20,20 +21,22 @@ public:
  * @param age
  * @param student_id
  */
-void Student(string first_name, string last_name, float age, string student_id);
-    
-string getStudentID();
+Student(const std::string& first_name, const std::string& last_name, float age, std::string student_id);
+
+std::string getStudentID() const;
     
 /**
  * @param value
  */
-void setStudentID(string value);
+void setStudentID(const std::string& value);
     
-void Studies();
-    
-string ToString();
-private: 
-    string m_studentID;
+void Studies() const;
+
+std::string ToString() override;
+
+private:
+	std::string m_studentID;
+std::string student_id_;
 };
 
 #endif //_STUDENT_H
