1085973d7f2d462dedca2b5798620c4366a24847
Devanshi <pdevanshi616@gmail.com>
2021-09-29 01:10:43 -0400

    Add project files.

---
+++ OOP3200-F2020-ICE3/Professor.cpp
@@ -0,0 +1,58 @@
+/**
+ * Project OOP3200-F2020-ICE3
+ * @author Devanshi Patel
+ * @version 1.0
+ */
+
+
+#include "Professor.h"
+
+/**
+ * Professor implementation
+ */
+
+
+/**
+ * @param first_name
+ * @param last_name
+ * @param age
+ * @param employee_ID
+ */
+void Professor::Professor(string first_name, string last_name, float age, string employee_ID) {
+
+}
+
+/**
+ * @return string
+ */
+string Professor::getEmployeeID() {
+    return "";
+}
+
+/**
+ * @param value
+ */
+void Professor::setEmployeeID(string value) {
+
+}
+
+/**
+ * @return void
+ */
+void Professor::Teaches() {
+    return;
+}
+
+/**
+ * @return void
+ */
+void Professor::ToString() {
+    return;
+}
+
+/**
+ * @return string
+ */
+string Professor::ToString() {
+    return "";
+}
